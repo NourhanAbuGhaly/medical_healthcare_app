88f37cb067aa9f89a5a002511b81ba5d3ef57c0b
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-07-08 09:31:33 +0300

    medical_healthcare_app

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: SingleChildScrollView(
+          child: Container(
+      width: MediaQuery.of(context).size.width,
+    //  height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(10),
+      child: Column(
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //     context, MaterialPageRoute(builder: (context) =>))
+                },
+                child: Text(
+                  "skip".toLowerCase(),
+                  style: TextStyle(color: Color(0xFF7165D6), fontSize: 20),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Image.asset("images/doctors.png"),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Doctors Appointment",
+              style: TextStyle(
+                  color: Color(0xFF7165D6),
+                  fontSize: 35,
+                  letterSpacing: 1,
+                  wordSpacing: 2),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Appoint Your doctor",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Material(
+                  color: Color(0xFF7165D6),
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {
+                      // Navigator.push(
+                      //     context, MaterialPageRoute(builder: (context) =>))
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                      child: Text("Log IN", style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+      ),
+    ),
+        ));
+  }
+}
