59664e669278a19e5be844af685db7bf143c689d
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-07-08 20:16:35 +0300

    medical_healthcare_app2

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool passTogle = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Image.asset("images/doctors.png"),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Full Name",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Email  Address",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                child: TextField(
+                  obscureText: passTogle ? true : false,
+                  decoration: InputDecoration(
+                      labelText: "Phone Number",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
