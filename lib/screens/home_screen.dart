592e70d1d6ed301ad32a8d44a5e240d4dfd50f42
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-07-19 12:56:44 +0300

    medical_healthcare_app5

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,180 +24,247 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.only(top: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Hello Alex",
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 35),
-                  ),
-                  CircleAvatar(
-                    radius: 25,
-                    backgroundImage: AssetImage("images/doctor1.jpg"),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(top: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF7165D6),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 6,
-                            spreadRadius: 4)
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              color: Colors.white, shape: BoxShape.circle),
-                          child: Icon(
-                            Icons.add,
-                            color: Color(0xFF7165D6),
-                            size: 35,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          "Circle Visit",
-                          style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Make an appointment",
-                          style: TextStyle(color: Colors.white54),
-                        )
-                      ],
-                    ),
-                  ),
+                Text(
+                  "Hello Alex",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 35),
                 ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 6,
-                            spreadRadius: 4)
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              color: Color(0xFF0EEFA), shape: BoxShape.circle),
-                          child: Icon(
-                            Icons.home_filled,
-                            color: Color(0xFF7165D6),
-                            size: 35,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          "Home Visit",
-                          style: TextStyle(
-                              fontSize: 18,
-                              //    color: Colors.black,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Call the doctor home",
-                          style: TextStyle(color: Colors.black54),
-                        )
-                      ],
-                    ),
-                  ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("images/doctor1.jpg"),
                 )
               ],
             ),
-            SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                "What are your symptoms? ",
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 23,
-                  color: Colors.black54,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 70,
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: symptoms.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      padding: EdgeInsets.symmetric(horizontal: 25),
-                      decoration: BoxDecoration(
-                          color: Color(0xFFF4F6FA),
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 4,
-                                spreadRadius: 2)
-                          ]),
-                      child: Center(
-                        child: Text(
-                          symptoms[index],
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black54),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF7165D6),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12, blurRadius: 6, spreadRadius: 4)
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.white, shape: BoxShape.circle),
+                        child: Icon(
+                          Icons.add,
+                          color: Color(0xFF7165D6),
+                          size: 35,
                         ),
                       ),
-                    );
-                  }),
-            )
-          ],
-        ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Circle Visit",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Make an appointment",
+                        style: TextStyle(color: Colors.white54),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12, blurRadius: 6, spreadRadius: 4)
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Color(0xFF0EEFA), shape: BoxShape.circle),
+                        child: Icon(
+                          Icons.home_filled,
+                          color: Color(0xFF7165D6),
+                          size: 35,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Home Visit",
+                        style: TextStyle(
+                            fontSize: 18,
+                            //    color: Colors.black,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Call the doctor home",
+                        style: TextStyle(color: Colors.black54),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Text(
+              "What are your symptoms? ",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 23,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 70,
+            child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: symptoms.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF4F6FA),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 4,
+                              spreadRadius: 2)
+                        ]),
+                    child: Center(
+                      child: Text(
+                        symptoms[index],
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Text(
+              "Popular Doctor ",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black54,
+                  fontSize: 23),
+            ),
+          ),
+          GridView.builder(
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: 4,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 4,
+                              spreadRadius: 2)
+                        ]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage("images/${imge[index]}"),
+                        ),
+                        Text(
+                          "Dr .Doctor Name",
+                          style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18),
+                        ),
+                        Text(
+                          " Therapist",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            Text(
+                              "4.9",
+                              style: TextStyle(color: Colors.black45),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              })
+        ],
       ),
     );
   }
